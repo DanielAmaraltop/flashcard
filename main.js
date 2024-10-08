5bfd476fc589c5ec1f18064b9726de537723afbf
Daniel Felipe  Dias do Amaral Galdino <107001396+DanielAmaraltop@users.noreply.github.com>
2024-10-08 08:38:49 -0300

    a

--- main.js
+++ main.js
@@ -3,32 +3,24 @@ function criaCartao(categoria, pergunta, resposta) {
     let cartao = document.createElement('article');
     cartao.className = 'cartao';
 
-    cartao.innerHTML = `
-    <div class="flashcard" onclick="this.classList.toggle('flip')">
-        <div class="cartao-pergunta question">
-            <h3>${categoria}</h3>
-            <p>${pergunta}</p>
-        </div>
-        <div class="cartao-resposta answer">
-            <p>${resposta}</p>
-        </div>
-    </div>
-    `;
+    function criaCartao(categoria, pergunta, resposta) {
+        let container = document.getElementById('container');
+        let cartao = document.createElement('article');
+        cartao.className = 'cartao';
     
-    container.appendChild(cartao);
-<div id="container">
-    <div class="flashcard" onclick="this.classList.toggle('flip')">
-        <div class="flashcard-inner">
-            <div class="cartao-pergunta">
-                <h3>Pergunta</h3>
-                <p>Qual é a capital da França?</p>
+        cartao.innerHTML = `
+        <div class="flashcard" onclick="this.classList.toggle('flip')">
+            <div class="cartao-pergunta question">
+                <h3>${categoria}</h3>
+                <p>${pergunta}</p>
             </div>
-            <div class="cartao-resposta">
-                <p>Paris</p>
+            <div class="cartao-resposta answer">
+                <p>${resposta}</p>
             </div>
         </div>
-    </div>
-</div>
+        `;
+        
+        container.appendChild(cartao);
 
 }
 
